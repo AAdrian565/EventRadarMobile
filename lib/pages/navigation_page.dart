5df8fcd1de1401863722fd432444a32242db2ae2
= <adrianagustinust@gmail.com>
2024-03-08 09:30:09 +0700

    Bottom Navigation Bar

---
+++ lib/pages/navigation_page.dart
@@ -0,0 +1,89 @@
+import 'package:event_radar2/pages/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationPage extends StatefulWidget {
+  const NavigationPage({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationPage> createState() => _NavigationPage();
+}
+
+class _NavigationPage extends State<NavigationPage> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: buildBody(),
+      bottomNavigationBar: buildNavigationBar(),
+    );
+  }
+
+  Widget buildBody() {
+    return SafeArea(
+      child: IndexedStack(
+        index: currentPageIndex,
+        children: const <Widget>[
+          HomePage(),
+          HomePage(), // NOTE: Add some page here
+          HomePage(), // NOTE: Add some page here
+          HomePage(), // NOTE: Add some page here
+        ],
+      ),
+    );
+  }
+
+  Widget buildNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 10,
+            spreadRadius: 1,
+            offset: Offset(0, -2),
+          ),
+        ],
+      ),
+      child: buildNavigationBarWidget(currentPageIndex),
+    );
+  }
+
+  Widget buildNavigationBarWidget(int currentIndex) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.white,
+      selectedItemColor: Colors.blue[800],
+      unselectedItemColor: Colors.black,
+      // showUnselectedLabels: false,
+      onTap: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+      currentIndex: currentIndex,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home_filled),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month_outlined),
+          activeIcon: Icon(Icons.calendar_month),
+          label: 'Schedule',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications_outlined),
+          activeIcon: Icon(Icons.notifications),
+          label: 'Notifications',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_outlined),
+          activeIcon: Icon(Icons.person_2_sharp),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
